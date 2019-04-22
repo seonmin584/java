0cb0b02ec979fb1049846c3120ea81c0548d2a0f
USER <USER@AMP-16>
2019-04-22 20:16:26 +0900

    Commit

---
+++ chap11-ex05/src/CheckBoxItemEventEx.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class CheckBoxItemEventEx extends JFrame {
+	private JCheckBox [] fruits = new JCheckBox [3];
+ 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
