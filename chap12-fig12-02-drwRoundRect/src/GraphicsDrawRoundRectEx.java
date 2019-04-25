f789f878aa1f94b3b65c0ea668374217984d09c4
USER <USER@AMP-16>
2019-04-25 18:05:54 +0900

    Commit

---
+++ chap12-fig12-02-drwRoundRect/src/GraphicsDrawRoundRectEx.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsDrawRoundRectEx extends JFrame{
+	private MyPanel panel = new MyPanel();
+	
+	public GraphicsDrawRoundRectEx() {
+		setTitle("°­¼±¹Î");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(200, 170);
+		setVisible(true);
+	}
+	
+	class MyPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(Color.RED);
+			g.drawRoundRect(20, 20, 120, 80, 40, 60);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GraphicsDrawRoundRectEx();
+	}
+
+}
