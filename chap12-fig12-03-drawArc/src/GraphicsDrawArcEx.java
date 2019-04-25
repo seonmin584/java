eac711d18c06e007f8eae57f58d3d2d38989dc17
USER <USER@AMP-16>
2019-04-25 18:06:13 +0900

    Commit

---
+++ chap12-fig12-03-drawArc/src/GraphicsDrawArcEx.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsDrawArcEx extends JFrame{
+	private MyPanel panel = new MyPanel();
+	public GraphicsDrawArcEx() {
+		setTitle("°­¼±¹Î");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(200,300);
+		setVisible(true);
+	}
+	
+	class MyPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(Color.RED);
+			g.drawArc(20, 100, 80, 80, 90, 270);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GraphicsDrawArcEx();
+	}
+
+}
