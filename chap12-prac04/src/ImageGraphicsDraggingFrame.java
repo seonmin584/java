eddd6aad301daaafd9ec3e42e6f9558140592b68
USER <USER@AMP-16>
2019-04-26 20:07:00 +0900

    Commit

---
+++ chap12-prac04/src/ImageGraphicsDraggingFrame.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ImageGraphicsDraggingFrame extends JFrame {
+	public ImageGraphicsDraggingFrame() {
+		super
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
